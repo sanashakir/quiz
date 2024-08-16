07d5ed9f98f1352787d6288a643e5023863c2022
Sana Shakir <sanashakir105@gmail.com>
2024-08-16 19:35:51 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,60 @@
+#! /usr/bin/env node
+
+import chalk from "chalk";
+import inquirer from "inquirer";
+
+const apiLink:string = "https://opentdb.com/api.php?amount=5&category=18&difficulty=easy"
+
+let fetchData = async (data:string) => {
+  let fetchQuiz:any = await fetch(data)
+  let res = await fetchQuiz.json();
+  return res.results;
+};
+let data = await fetchData(apiLink);
+let startQuiz = async () => {
+  let score : number = 0;
+  let name = await inquirer.prompt({
+    name:"askName",
+    type:"input",
+    message:"What is your name?"
+  });
+
+  for(let i=0; i<5; i++){
+    let answers = [...data[i].incorrect_answers, data[i].correct_answer];
+  
+  let ans = await inquirer.prompt({
+    type:"list",
+    name:"quiz",
+    message : data[i].question,
+    choices : answers.map((val:any)=>val),
+  });
+
+ if(ans.quiz == data[i].correct_answer){
+  ++score 
+  console.log(chalk.blueBright("Correct")); 
+ } else{
+  console.log(`correct answer is ${chalk.red( data[i].correct_answer)}`);
+  
+ }
+}
+console.log(`Dear ${chalk.green.bold(name.askName)}, yuor score is ${chalk.redBright(score)} out of${chalk.red(" 5 ")}`);
+};
+startQuiz();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
